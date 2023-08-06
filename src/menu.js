f171f53600b691b3f3d8e7905ee3cf195369de56
TCdotmake <tommyjchen@hotmail.com>
2023-08-06 00:23:24 -0400

    started to add style to menu

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,45 @@
+import juiceObjArr from "./juiceRecipe";
+const menuDiv = document.createElement("div");
+menuDiv.setAttribute("id", "menuDiv");
 
 
-const menuDiv = document.createElement('div');
-menuDiv.setAttribute('id', 'menuDiv');
-const label = document.createElement('h1');
-label.innerHTML = 'menuDiv';
-menuDiv.appendChild(label);
+const juiceMenuDiv = document.createElement('div');
+juiceMenuDiv.setAttribute('id', 'juiceMenu');
+juiceMenuDiv.classList.add('menuSection');
+const juiceSubHeading = document.createElement('h2');
+juiceSubHeading.innerHTML = 'Cold Pressed Juice';
+juiceSubHeading.classList.add('menuSubHeading');
+juiceMenuDiv.appendChild(juiceSubHeading);
+const juiceMenuList = document.createElement('ul');
+juiceMenuDiv.appendChild(juiceMenuList);
+for(let obj of juiceObjArr){
+    const menuItem = document.createElement('li');
+    menuItem.classList.add('menuItem');
+    
+    //item name
+    const itemName = document.createElement('h3');
+    itemName.innerHTML = obj.name;
+    itemName.classList.add('itemName');
+    menuItem.appendChild(itemName);
+    
+    //list of ingredients
+    const ingredientList = document.createElement('ul');
+    ingredientList.classList.add('ingredientList');
+    for(let n of obj.ingredients){
+        const ingredient = document.createElement('li');
+        ingredient.innerHTML = n;
+        ingredient.classList.add('ingredient');
+        ingredientList.appendChild(ingredient);
+    }
+    menuItem.appendChild(ingredientList);
 
+    //description
+    const itemDescrip = document.createElement('div');
+    itemDescrip.innerHTML = obj.description;
+    itemDescrip.classList.add('itemDescrip');
+    menuItem.appendChild(itemDescrip);
+
+    juiceMenuList.appendChild(menuItem);
+}
+menuDiv.appendChild(juiceMenuDiv);
 export default menuDiv;
