83b3fba53c0504542789340d3b9d54da815970fd
Tommy Chen <tommyjchen@hotmail.com>
2023-08-12 17:38:29 -0400

    logo section done

--- src/header.js
+++ src/header.js
@@ -8,13 +8,29 @@ import contactDiv from './contact.js';
 const header = document.createElement('div');
 header.setAttribute('id', 'header');
 
-//place holder for logo
 
-const logo = document.createElement('h1');
-logo.innerHTML = 'Logo Placeholder';
-
+//logo
+const coldText = document.createElement('h2');
+coldText.classList.add('coldPressSub');
+coldText.setAttribute('id', 'coldText');
+coldText.innerHTML='COLD';
+const pressText = document.createElement('h2');
+pressText.classList.add('coldPressSub');
+pressText.setAttribute('id','pressText');
+pressText.innerHTML="PRESS";
+const logoSubDiv = document.createElement('div');
+logoSubDiv.setAttribute('id','logoSubDiv');
+logoSubDiv.appendChild(coldText);
+logoSubDiv.appendChild(pressText);
+const kitchenText = document.createElement('h1');
+kitchenText.setAttribute('id','kitchenText');
+kitchenText.innerHTML='KITCHEN';
 const logoDiv = document.createElement('div');
-logoDiv.innerHTML= logo;
+logoDiv.setAttribute('id','logoDiv');
+logoDiv.appendChild(logoSubDiv);
+logoDiv.appendChild(kitchenText);
+
+header.appendChild(logoDiv);
 //navigation bar
 
 const homeBtn = document.createElement('li');
@@ -33,7 +49,7 @@ const navBar = document.createElement('nav');
 navBar.setAttribute('id','navBar');
 navBar.appendChild(navUl);
 
-header.appendChild(logo);
+
 header.appendChild(navBar);
 
 homeBtn.addEventListener('click', ()=>route(contentDiv, homeDiv));
