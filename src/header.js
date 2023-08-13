5c9ed591508523af54b5b5bbfed05ebc6dd9c4fe
Tommy Chen <tommyjchen@hotmail.com>
2023-08-12 20:52:44 -0400

    reworked route to include tab highlight on navbar, header fully functional

--- src/header.js
+++ src/header.js
@@ -1,66 +1,47 @@
+import route from "./router.js";
+import contentDiv from "./content.js";
+import homeDiv from "./home.js";
+import menuDiv from "./menu.js";
+import contactDiv from "./contact.js";
+import LogoImg from "./img/Screenshot 2023-08-12 at 7.09.10 PM.png";
 
-import route from './router.js';
-import contentDiv from './content.js';
-import homeDiv from './home.js';
-import menuDiv from './menu.js';
-import contactDiv from './contact.js';
-import LogoImg from './img/Screenshot 2023-08-12 at 7.09.10 PM.png'
-
-const header = document.createElement('div');
-header.setAttribute('id', 'header');
-
-
-//logo using css
-// const coldText = document.createElement('h2');
-// coldText.classList.add('coldPressSub');
-// coldText.setAttribute('id', 'coldText');
-// coldText.innerHTML='COLD';
-// const pressText = document.createElement('h2');
-// pressText.classList.add('coldPressSub');
-// pressText.setAttribute('id','pressText');
-// pressText.innerHTML="PRESS";
-// const logoSubDiv = document.createElement('div');
-// logoSubDiv.setAttribute('id','logoSubDiv');
-// logoSubDiv.appendChild(coldText);
-// logoSubDiv.appendChild(pressText);
-// const kitchenText = document.createElement('h1');
-// kitchenText.setAttribute('id','kitchenText');
-// kitchenText.innerHTML='KITCHEN';
-// const logoDiv = document.createElement('div');
-// logoDiv.setAttribute('id','logoDiv');
-// logoDiv.appendChild(logoSubDiv);
-// logoDiv.appendChild(kitchenText);
-
-//header.appendChild(logoDiv);
-//navigation bar
+const header = document.createElement("div");
+header.setAttribute("id", "header");
 
 //logo img
 const logoImg = new Image();
 logoImg.src = LogoImg;
-logoImg.setAttribute('id','logoImg');
+logoImg.setAttribute("id", "logoImg");
 header.appendChild(logoImg);
 
-const homeBtn = document.createElement('li');
-homeBtn.innerHTML= 'Home';
-const menuBtn = document.createElement('li');
-menuBtn.innerHTML= "Menu";
-const contactBtn = document.createElement('li');
-contactBtn.innerHTML = 'Contact';
-
-
-const navUl = document.createElement('ul');
+//navigation btn
+const homeBtn = document.createElement("li");
+homeBtn.innerHTML = "Home";
+homeBtn.setAttribute("id", "homeBtn");
+homeBtn.classList.add("selected"); //home highlighted on page load
+const menuBtn = document.createElement("li");
+menuBtn.innerHTML = "Menu";
+menuBtn.setAttribute("id", "menuBtn");
+const contactBtn = document.createElement("li");
+contactBtn.innerHTML = "Contact";
+contactBtn.setAttribute("id", "contactBtn");
+const btnArr = [homeBtn, menuBtn, contactBtn];
+for (let n of btnArr) {
+  n.classList.add("navBtn");
+}
+//navigation bar
+const navUl = document.createElement("ul");
 navUl.appendChild(homeBtn);
 navUl.appendChild(menuBtn);
 navUl.appendChild(contactBtn);
-const navBar = document.createElement('nav');
-navBar.setAttribute('id','navBar');
+const navBar = document.createElement("nav");
+navBar.setAttribute("id", "navBar");
 navBar.appendChild(navUl);
-
-
 header.appendChild(navBar);
 
-homeBtn.addEventListener('click', ()=>route(contentDiv, homeDiv));
-menuBtn.addEventListener('click', ()=>route(contentDiv, menuDiv));
-contactBtn.addEventListener('click', ()=>route(contentDiv, contactDiv));
+//event listeners for navigation buttons
+homeBtn.addEventListener("click", route);
+menuBtn.addEventListener("click", route);
+contactBtn.addEventListener("click", route);
 
 export default header;
--- src/router.js
+++ src/router.js
@@ -1,15 +1,33 @@
-//router
-//  arguments: (parent, child)
-//  takes in a container/parent element
-//  empty it and append child from argument
+import contentDiv from "./content";
+import homeDiv from "./home";
+import menuDiv from "./menu";
+import contactDiv from "./contact";
 
-const route = (parent, child) =>{
-    if(parent.hasChildNodes){
-        while(parent.firstChild){
-            parent.removeChild(parent.lastChild);
-        }
+//change tab focus
+function changeTabColor(event){
+    const parent = event.currentTarget.parentNode;
+    for (let child of parent.children) {
+      child.classList.remove("selected");
     }
-    parent.appendChild(child);
+    event.currentTarget.classList.add("selected");
+}
+
+//empty and repopulate content
+function route(event) {
+  changeTabColor(event);
+  if (contentDiv.hasChildNodes) {
+    while (contentDiv.firstChild) {
+      contentDiv.removeChild(contentDiv.lastChild);
+    }
+  }
+  const id = event.target.id;
+  if (id == "homeBtn") {
+    contentDiv.appendChild(homeDiv);
+  } else if (id == "menuBtn") {
+    contentDiv.appendChild(menuDiv);
+  } else if (id == "contactBtn") {
+    contentDiv.appendChild(contactDiv);
+  }
 }
 
 export default route;
