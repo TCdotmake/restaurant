ae5f9480e73de2341d80995ebf2e864180fc0d64
Tommy Chen <tommyjchen@hotmail.com>
2023-08-20 21:14:50 -0400

    additional info section added to ontact

--- src/contact.js
+++ src/contact.js
@@ -19,7 +19,7 @@ signUpDiv.appendChild(signUpHeader);
 //sign up sub
 const signUpSub = document.createElement('p');
 signUpSub.innerHTML = 'Get updates on new items and special events';
-signUpSub.setAttribute('id', 'signUpSub');
+signUpSub.classList.add('contactSub');
 
 signUpDiv.appendChild(signUpSub);
 
@@ -47,13 +47,80 @@ submit.setAttribute('type','submit');
 submit.setAttribute('value', 'Subscribe');
 submit.setAttribute('id', 'submit');
 
-
 emailForm.appendChild(emailDiv);
-
 emailForm.appendChild(submit);
-
 signUpDiv.appendChild(emailForm);
 
 contactDiv.appendChild(signUpDiv); 
+
+// contact info container
+const contactInfoDiv = document.createElement('div');
+contactInfoDiv.setAttribute('id', "contactInfoDiv");
+
+// hours
+const dayArr = [
+    'Mon - Fri:',
+    'Saturday:',
+    'Sunday:'
+];
+
+const hourArr = [
+    '7AM - 4PM',
+    '7AM - 6PM',
+    '7AM - 2PM'
+];
+
+const hourDiv = document.createElement('div');
+hourDiv.classList.add('contactDiv');
+// sub heading
+const hourSub = document.createElement('h3');
+hourSub.classList.add('contactSub');
+hourSub.innerHTML='Opening Hours';
+hourDiv.appendChild(hourSub);
+
+// hour list
+const hourList = document.createElement('ul');
+
+for(let i in dayArr){
+    const item = document.createElement('li');
+    item.classList.add('contactItem');
+    item.classList.add('smallText');
+    const day = document.createElement('p');
+    day.innerHTML=dayArr[i];
+    const hour = document.createElement('p');
+    hour.innerHTML=hourArr[i];
+    item.appendChild(day);
+    item.appendChild(hour);
+    hourList.append(item);
+}
+hourDiv.appendChild(hourList);
+
+contactInfoDiv.appendChild(hourDiv);
+
+//contact
+const detailsDiv = document.createElement('div');
+detailsDiv.classList.add('contactDiv');
+detailsDiv.classList.add('smallText');
+const detailSub = document.createElement('h3');
+detailSub.classList.add('contactSub');
+detailSub.innerHTML = 'Contact';
+detailsDiv.appendChild(detailSub);
+const phone = document.createElement('p');
+phone.innerHTML = '(123) 456-7890';
+detailsDiv.appendChild(phone);
+const email = document.createElement('p');
+email.innerHTML = 'info@coldpressedkitchen.com';
+detailsDiv.appendChild(email);
+const address1 = document.createElement('p');
+address1.innerHTML = '915 Wiegand Road';
+detailsDiv.appendChild(address1);
+const address2 = document.createElement('p');
+address2.innerHTML = 'Port Mariettahaven, NE 60448';
+detailsDiv.appendChild(address2);
+
+contactInfoDiv.appendChild(detailsDiv);
+
+contactDiv.appendChild(contactInfoDiv);
+
 
 export default contactDiv;
